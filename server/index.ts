d8214d5eb9277da1f4d7081a433cad589fa7a99a
fanghaonan <haonan.2000@outlook.com>
2025-12-31 19:21:06 +0800

    chore(trpc): register files router

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,10 @@
 import { router } from "./trpc";
 import { conversationRouter } from "./routers/conversation";
+import { filesRouter } from "./routers/files";
 
 export const appRouter = router({
   conversation: conversationRouter,
+  files: filesRouter,
 });
 
 export type AppRouter = typeof appRouter;
-
